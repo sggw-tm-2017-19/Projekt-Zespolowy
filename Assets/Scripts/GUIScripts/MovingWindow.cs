95c1ea95496a12aea3be61dd42837f8f2bf05954
Patryk <patryk2300@gmail.com>
2018-04-27 22:03:57 +0200

    Dodanie możliwości przeciągania menu ekwipunku oraz questlogu, nadanie funkcji buttonom statystyk

---
+++ Assets/Scripts/GUIScripts/MovingWindow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class MovingWindow : MonoBehaviour, IPointerDownHandler, IDragHandler {
+    private Vector2 pointerOffset;
+    private RectTransform canvasRectTransform;
+    private RectTransform gameObjectRectTransform;
+    void Awake()
+    {
+        Canvas canvas = GetComponentInParent<Canvas>();
+        canvasRectTransform = canvas.transform as RectTransform;
+        gameObjectRectTransform = transform as RectTransform;
+    }
+    public void OnPointerDown (PointerEventData data)
+    {
+        gameObjectRectTransform.SetAsLastSibling();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(gameObjectRectTransform, data.position, data.pressEventCamera, out pointerOffset);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (gameObjectRectTransform == null)
+            return;
+
+        Vector2 localPointerPosition;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle (
+            canvasRectTransform, eventData.position,eventData.pressEventCamera,out localPointerPosition))
+        {
+            gameObjectRectTransform.localPosition = localPointerPosition - pointerOffset;
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/GUIScripts/StatsUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatsUI : MonoBehaviour{
+    public List<TextMeshProUGUI> attributes;
+    public void AddNumber(TextMeshProUGUI attr)
+    {
+        int value = Convert.ToInt32(attr.text);
+        value++;
+        attributes[attributes.IndexOf(attr)].text = value.ToString();
+    }
+    public void SubtractNumber(TextMeshProUGUI attr)
+    {
+        int value = Convert.ToInt32(attr.text);
+        if(value > 0)
+            value--;
+        attributes[attributes.IndexOf(attr)].text = value.ToString();
+    }
+}
