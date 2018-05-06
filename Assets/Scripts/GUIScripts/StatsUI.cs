405dccd72a675c8525bc14e49762160e516c719d
Patryk <patryk2300@gmail.com>
2018-05-06 21:24:11 +0200

    Dodanie minimapy, niewielkie zmiany w inventory

--- Assets/Scripts/GUIScripts/StatsUI.cs
+++ Assets/Scripts/GUIScripts/StatsUI.cs
@@ -6,17 +6,17 @@ using TMPro;
 
 public class StatsUI : MonoBehaviour{
     public List<TextMeshProUGUI> attributes;
+    public TextMeshProUGUI statsPointNumber;
     public void AddNumber(TextMeshProUGUI attr)
     {
-        int value = Convert.ToInt32(attr.text);
-        value++;
-        attributes[attributes.IndexOf(attr)].text = value.ToString();
-    }
-    public void SubtractNumber(TextMeshProUGUI attr)
-    {
-        int value = Convert.ToInt32(attr.text);
-        if(value > 0)
-            value--;
-        attributes[attributes.IndexOf(attr)].text = value.ToString();
+        if (Convert.ToInt32(statsPointNumber.text) > 0)
+        {
+            int number = Convert.ToInt32(statsPointNumber.text);
+            int value = Convert.ToInt32(attr.text);
+            value++;
+            attributes[attributes.IndexOf(attr)].text = value.ToString();
+            number -= 1;
+            statsPointNumber.text = number.ToString();
+        }
     }
 }
