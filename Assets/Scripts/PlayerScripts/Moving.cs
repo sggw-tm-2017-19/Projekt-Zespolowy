f303fb2d5979607754687b82899d2a2148e16182
DubmasterxD <dubmaster@tlen.pl>
2018-05-10 02:15:35 +0200

    Dodanie możliwości "rozmowy" z NPC

--- Assets/Scripts/PlayerScripts/Moving.cs
+++ Assets/Scripts/PlayerScripts/Moving.cs
@@ -4,52 +4,77 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-	private Rigidbody2D rb;
+    private Rigidbody2D rb;
     private SpriteRenderer playerSprite;
     float tempMove, jumpSpeed = 50, moveSpeed = 8;
-	Vector2 jump, move;
-	private bool isGrounded;
+    Vector2 jump, move;
+    private bool isGrounded, canWalk;
     Animator anim;
+    ContactFilter2D cFilt = new ContactFilter2D();
+    private Collider2D[] overlapped;
 
-	// Use this for initialization
-	void Start()
-	{
-		rb = GetComponent<Rigidbody2D>();
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
         playerSprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-	}
+        canWalk = true;
+        overlapped = new Collider2D[10];
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-        if (isGrounded)
+    // Update is called once per frame
+    void Update()
+    {
+        if (canWalk)
         {
-            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
-                tempMove = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-            else
-                tempMove = 0;
-            if (tempMove < 0) playerSprite.flipX = true;
-            if (tempMove > 0) playerSprite.flipX = false;
-            anim.SetFloat("Speed", Mathf.Abs(tempMove));
-            move = new Vector2(tempMove, 0);
+            //Walk
+            if (isGrounded)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+                    tempMove = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+                else
+                    tempMove = 0;
+                if (tempMove < 0) playerSprite.flipX = true;
+                if (tempMove > 0) playerSprite.flipX = false;
+                anim.SetFloat("Speed", Mathf.Abs(tempMove));
+                move = new Vector2(tempMove, 0);
+            }
+            rb.transform.Translate(move[0], 0, 0);
+            //Jump
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+            {
+                jump = new Vector2(0, jumpSpeed);
+                rb.AddForce(jump, ForceMode2D.Impulse);
+                rb.AddForce(move);
+                isGrounded = false;
+                anim.SetBool("Grounded", isGrounded);
+            }
         }
-        rb.transform.Translate(move[0], 0, 0);
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
-		{
-			jump = new Vector2(0, jumpSpeed);
-			rb.AddForce(jump, ForceMode2D.Impulse);
-            rb.AddForce(move);
-			isGrounded = false;
-            anim.SetBool("Grounded", isGrounded);
+        //Interaction
+        if (Input.GetKeyDown(KeyCode.Space) && tempMove == 0 && isGrounded)
+        {
+            rb.OverlapCollider(cFilt, overlapped);
+            //NPC
+            if (overlapped[0].gameObject.layer == 10)
+            {
+                overlapped[0].gameObject.GetComponent<NPCInteraction>().Test();
+            }
         }
     }
 
-	private void OnCollisionEnter2D(Collision2D col)
-	{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        //Land
         if (col.gameObject.layer == 9)
         {
             isGrounded = true;
             anim.SetBool("Grounded", isGrounded);
         }
+    }
+
+    public void SetCanWalk(bool choice)
+    {
+        canWalk = choice;
     }
 }
