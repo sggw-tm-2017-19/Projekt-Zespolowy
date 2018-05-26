e3270f308ad327121d8b8a000b328ae71a423d46
Patryk <patryk2300@gmail.com>
2018-05-26 20:28:30 +0200

    Dodano przyblizanie i oddalanie w minimapie oraz sklepu z możliwością ulepszania przedmiotów

---
+++ Assets/Scripts/GUIScripts/ZoomScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomScript : MonoBehaviour{
+
+    public Camera miniMapCam;
+
+    public void ZoomIn()
+    {
+        if (miniMapCam.orthographicSize >= 8)
+        {
+            miniMapCam.orthographicSize -= 2;
+        }
+    }
+    public void ZoomOut()
+    {
+        if (miniMapCam.orthographicSize <= 30)
+        {
+            miniMapCam.orthographicSize += 2;
+        }
+    }
+}
