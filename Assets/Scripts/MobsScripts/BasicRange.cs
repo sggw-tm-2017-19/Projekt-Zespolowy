4bc75642066099b4f13f231628f3af3962c91789
tkondraciuk <tomek.kondraciuk@op.pl>
2018-05-31 14:44:09 +0200

    Teraz Sukkub strzela ognistymi pociskami. Pociski lecą po linii prostej w stronę punku gdzie stała postać gracza w momencie gdy kula się pojawiła.

---
+++ Assets/Scripts/MobsScripts/BasicRange.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRange : MonoBehaviour {
+    public GameObject projectilePrefab;
+    public float Cooldown;
+    public int NumberOfProjectiles=1;
+    public float Range;
+    public Transform ProjectileStartPoint;
+
+    private BoxCollider2D boxCollider;
+    private float timeCount = 0;
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        boxCollider = GetComponent<BoxCollider2D>();
+        setColliderSize();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCount += Time.deltaTime;
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.name == "Player" && timeCount>=Cooldown)
+        {
+            Attack(other);
+            timeCount = 0;
+        }
+    }
+
+    private void Attack(GameObject other)
+    {
+        Vector2 direction = player.transform.position - ProjectileStartPoint.position;
+        GameObject projectile = Instantiate(projectilePrefab);
+        projectile.transform.position = ProjectileStartPoint.position;
+        projectile.SendMessage("setDirection", direction);
+    }
+
+    void setColliderSize()
+    {
+        boxCollider.size += new Vector2(Range / 2, 0);
+        boxCollider.offset -= new Vector2(Range / 2, 0);
+    }
+}
--- Assets/Scripts/OtherScripts/ProjectileScript.cs
+++ Assets/Scripts/OtherScripts/ProjectileScript.cs
@@ -7,14 +7,21 @@ public class ProjectileScript : MonoBehaviour {
     public float speed;
     public float lifeTime;
     public int damage;
-    public Vector2 movementDirection;
+    public float directionAngle;
+
+    private Vector2 movementDirection;
 
     Vector3 Position { get { return transform.position; } set { transform.position = value; } }
 
-	// Use this for initialization
-	void Start () {
-        movementDirection.Normalize();
+    private void Awake()
+    {
+        setDirectionAngle(directionAngle);
+    }
+
+    // Use this for initialization
+    void Start () {
         GameObject.Destroy(gameObject, lifeTime);
+        //transform.rotation = 
 	}
 	
 	// Update is called once per frame
@@ -25,12 +32,24 @@ public class ProjectileScript : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject otherObject = collision.gameObject;
-        if (otherObject.name == "Player")
-        {
-            otherObject.SendMessage("TakeDamage", damage);
-            GameObject.Destroy(gameObject);
-        }
+        if (otherObject.name == "Player") otherObject.SendMessage("TakeDamage", damage);
+        if (otherObject.tag != "Enemy") Destroy(gameObject);
         
+    }
+
+    public void setDirectionAngle(float angle)
+    {
+        directionAngle = Mathf.Deg2Rad * angle;
+        movementDirection = new Vector2(Mathf.Cos(directionAngle), Mathf.Sin(directionAngle)).normalized;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    public void setDirection(Vector2 direction)
+    {
+        movementDirection = direction.normalized;
+        float angle = Vector2.SignedAngle(Vector2.right, movementDirection);
+        directionAngle = angle * Mathf.Deg2Rad;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
 
