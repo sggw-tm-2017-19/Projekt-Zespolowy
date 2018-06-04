101c1872fc7214e705fa363823394b14bee64a53
Klaudia <klaudia.piwowarczyk@azacode.co>
2018-06-04 23:23:19 +0200

    poprawki

--- Assets/Scripts/MobsScripts/MobsStats.cs
+++ Assets/Scripts/MobsScripts/MobsStats.cs
@@ -6,38 +6,14 @@ public class MobsStats : MonoBehaviour
 	private decimal moveSpeed, attackSpeed;
 	private int armor, damage, healthPoints, maxHP;
 
-	private enum Types { Melee, Ranged }
-	private enum Mobs { Imp, Skkub, Wizard }
+	private enum Types { Melee, Ranged, Boss }
+	private enum Bosses { Imp, Skkub, Wizard }
 	[SerializeField]
-	private Types Fight;
+	private Types Mob;
 	[SerializeField]
-	private Mobs Mob;
+	private Bosses Boss;
 	[SerializeField]
 	private int Level;
-
-
-	public void SaveState()
-	{
-		GlobalControl.Instance.attackSpeed = attackSpeed;
-		GlobalControl.Instance.moveSpeed = moveSpeed;
-		GlobalControl.Instance.armor = armor;
-		GlobalControl.Instance.damage = damage;
-		GlobalControl.Instance.healthPoints = healthPoints;
-		GlobalControl.Instance.maxHP = maxHP;
-	}
-
-	public void LoadState()
-	{
-		attackSpeed = GlobalControl.Instance.attackSpeed;
-		moveSpeed = GlobalControl.Instance.moveSpeed;
-		armor = GlobalControl.Instance.armor;
-		damage = GlobalControl.Instance.damage;
-		healthPoints = GlobalControl.Instance.healthPoints;
-		maxHP = GlobalControl.Instance.maxHP;
-
-
-		// transform.SetPositionAndRotation(new Vector3(GlobalControl.Instance.posX, GlobalControl.Instance.posY, GlobalControl.Instance.posZ), new Quaternion(0, 0, 0, 0));
-	}
 
 	/// <summary>
 	/// Zmiana statystyk po uderzeniu w moba
@@ -123,8 +99,42 @@ public class MobsStats : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		switch (Mobs[type]) {
-		case Mobs.Imp:
+		switch (Mob) {
+		case Types.Boss:
+			switch (Boss) {
+			case Bosses.Imp:
+				Level = 1;
+				moveSpeed = 1;
+				attackSpeed = 1;
+				armor = 1;
+				damage = 1;
+				healthPoints = 1;
+				maxHP = 1;
+				break;
+			case Bosses.Skkub:
+				Level = 1;
+				moveSpeed = 1;
+				attackSpeed = 1;
+				armor = 1;
+				damage = 1;
+				healthPoints = 1;
+				maxHP = 1;
+				break;
+			case Bosses.Wizard:
+				Level = 1;
+				moveSpeed = 1;
+				attackSpeed = 1;
+				armor = 1;
+				damage = 1;
+				healthPoints = 1;
+				maxHP = 1;
+				break;
+				
+			default:
+				break;
+			}
+
+		case Types.Melee:
 			Level = 1;
 			moveSpeed = 1;
 			attackSpeed = 1;
@@ -134,17 +144,7 @@ public class MobsStats : MonoBehaviour
 			maxHP = 1;
 			break;
 
-		case Mobs.Skkub:
-			Level = 1;
-			moveSpeed = 1;
-			attackSpeed = 1;
-			armor = 1;
-			damage = 1;
-			healthPoints = 1;
-			maxHP = 1;
-			break;
-
-		case Mobs.Wizard:
+		case Types.Ranged:
 			Level = 1;
 			moveSpeed = 1;
 			attackSpeed = 1;
