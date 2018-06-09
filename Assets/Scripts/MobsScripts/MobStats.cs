f7a914dd75522d0dc00f52f118e193eebd6b0fba
MateuszKub <mateusz.zyleta@wp.pl>
2018-06-09 09:29:32 +0200

    Dodano HP potworów + kontrola czy jest zestunowany

---
+++ Assets/Scripts/MobsScripts/MobStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobStats : MonoBehaviour {
+
+    public int MaxHP;
+    // Use this for initialization
+    void Start()
+    {
+        HealthPoints = MaxHP;
+        Stun = false;
+    }
+
+    public int HealthPoints { get; set; }
+
+    public bool Stun { get; set; }
+}
