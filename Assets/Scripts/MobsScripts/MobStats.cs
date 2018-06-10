0bb8828f41a340fd35335be12ca3007f7ac49883
DubmasterxD <dubmaster@tlen.pl>
2018-06-10 07:46:45 +0200

    Zmiana Statystyk mobów na wersję od Klaudii

--- Assets/Scripts/MobsScripts/MobStats.cs
+++ Assets/Scripts/MobsScripts/MobStats.cs
@@ -1,18 +1,174 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class MobStats : MonoBehaviour {
+public class MobsStats : MonoBehaviour
+{
+    private decimal moveSpeed, attackSpeed;
+    private int armor, damage, healthPoints, maxHP;
 
-    public int MaxHP;
+    private enum Types { Melee, Ranged, Boss }
+    private enum Bosses { Imp, Skkub, Wizard }
+    [SerializeField]
+    private Types Mob;
+    [SerializeField]
+    private Bosses Boss;
+    [SerializeField]
+    private int Level;
+
+    /// <summary>
+    /// Zmiana statystyk po uderzeniu w moba
+    /// </summary>
+    public void HealthPointsDown()
+    {
+        healthPoints -= 1;
+    }
+
+    //Getters & Setters
+    public decimal MoveSpeed
+    {
+        get
+        {
+            return moveSpeed;
+        }
+        set
+        {
+            moveSpeed = value;
+        }
+    }
+
+    public decimal AttackSpeed
+    {
+        get
+        {
+            return attackSpeed;
+        }
+        set
+        {
+            attackSpeed = value;
+        }
+    }
+
+    public int Armor
+    {
+        get
+        {
+            return armor;
+        }
+        set
+        {
+            armor = value;
+        }
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public int HealthPoints
+    {
+        get
+        {
+            return healthPoints;
+        }
+        set
+        {
+            healthPoints = value;
+        }
+    }
+
+    public int MaxHP
+    {
+        get
+        {
+            return maxHP;
+        }
+        set
+        {
+            maxHP = value;
+        }
+    }
+
+    public bool Stun { get; set; }
+
     // Use this for initialization
     void Start()
     {
-        HealthPoints = MaxHP;
         Stun = false;
+        switch (Mob)
+        {
+            case Types.Boss:
+                switch (Boss)
+                {
+                    case Bosses.Imp:
+                        Level = 1;
+                        moveSpeed = 1;
+                        attackSpeed = 1;
+                        armor = 1;
+                        damage = 1;
+                        healthPoints = 1;
+                        maxHP = 1;
+                        break;
+                    case Bosses.Skkub:
+                        Level = 1;
+                        moveSpeed = 1;
+                        attackSpeed = 1;
+                        armor = 1;
+                        damage = 1;
+                        healthPoints = 1;
+                        maxHP = 1;
+                        break;
+                    case Bosses.Wizard:
+                        Level = 1;
+                        moveSpeed = 1;
+                        attackSpeed = 1;
+                        armor = 1;
+                        damage = 1;
+                        healthPoints = 1;
+                        maxHP = 1;
+                        break;
+
+                    default:
+                        break;
+                }
+
+            case Types.Melee:
+                Level = 1;
+                moveSpeed = 1;
+                attackSpeed = 1;
+                armor = 1;
+                damage = 1;
+                healthPoints = 1;
+                maxHP = 1;
+                break;
+
+            case Types.Ranged:
+                Level = 1;
+                moveSpeed = 1;
+                attackSpeed = 1;
+                armor = 1;
+                damage = 1;
+                healthPoints = 1;
+                maxHP = 1;
+                break;
+
+            default:
+                break;
+        }
     }
 
-    public int HealthPoints { get; set; }
+    // Update is called once per frame
+    void Update()
+    {
 
-    public bool Stun { get; set; }
+    }
 }
+
